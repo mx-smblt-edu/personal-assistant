93b611ee4d0ff52c064783ef5dba26b62345871a
mx-smblt-edu <mx.smblt.edu@gmail.com>
2025-11-13 10:38:05 +0200

    PA-006 Implemented the Birthday class.
    Co-authored-by: mx-smblt <mx.smblt@gmail.com>

---
+++ src/error/invalid_birthday_error.py
@@ -0,0 +1,23 @@
+"""
+A module defining a custom exception for handling invalid birthday date formats.
+
+This module provides an `InvalidBirthdayError` exception for scenarios where
+a user provides a birthday date in an invalid format. The exception guides
+the user to use the correct `DD.MM.YYYY` format.
+"""
+from colorama import Fore, Style
+
+
+class InvalidBirthdayError(Exception):
+    """
+    Exception raised for invalid birthday date format.
+
+    This exception is raised when a birthday date is provided in an invalid
+    format. It informs the user to use the correct format `DD.MM.YYYY`.
+
+    :param date: The invalid birthday date string.
+    """
+
+    def __init__(self, date: str):
+        self.message = (f"{Fore.RED}[ERROR]{Style.RESET_ALL} Invalid date format '{date}'. "
+                        f"Use {Fore.GREEN}DD.MM.YYYY{Style.RESET_ALL}.")
---
+++ src/model/birthday.py
@@ -0,0 +1,42 @@
+"""
+Provides the Birthday class.
+"""
+from datetime import datetime, date
+
+from src.error.invalid_birthday_error import InvalidBirthdayError
+
+
+class Birthday:
+    """
+    Represents a birthday field.
+
+    This class provides functionality to store and handle a birthday
+    date in a specific format. It ensures that the provided date string
+    adheres to the expected format and converts it to a date object for
+    further use.
+    """
+    format = "%d.%m.%Y"
+
+    def __init__(self, value: str):
+        try:
+            date_of_birthday: date = datetime.strptime(value, Birthday.format).date()
+            self.__value = date_of_birthday
+        except ValueError as vex:
+            raise InvalidBirthdayError(value) from vex
+
+    @property
+    def value(self) -> date:
+        """Getter for the value of the birthday."""
+        return self.__value
+
+    def to_string(self) -> str:
+        """Returns the birthday as a string in the expected format."""
+        return self.__value.strftime(Birthday.format)
+
+    def __str__(self) -> str:
+        return f"Birthday: {self.to_string()}"
+
+    def __eq__(self, other) -> bool:
+        if type(self) != type(other):
+            raise TypeError("Cannot compare %r and %r" % (self, other))
+        return self.__value == other.__value
